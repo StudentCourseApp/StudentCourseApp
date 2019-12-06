ab06dbf3e6337453c35b1979ebe4611056addaf0
Soheila Escobar <soheilaescobar@Soheilas-MacBook-Pro.local>
2019-12-06 14:41:32 -0500

    course

--- src/main/java/com/roselake/jbc/HomeControllerSoheila.java
+++ src/main/java/com/roselake/jbc/HomeControllerSoheila.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -21,6 +18,40 @@ public class HomeControllerSoheila {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    CourseRepository courseRepository;
 
+    @GetMapping("/addcourse")
+    public String courseForm(Model model){
+        model.addAttribute("course", new Course());
+        return "courseform";
+    }
+
+    @GetMapping("/editcourse/{id}")
+    public String editCourse(@PathVariable("id") Long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "courseform";
+    }
+
+    @GetMapping("/deletecourse/{id}")
+    public String deleteCourse(@PathVariable("id") Long id, Model model){
+        courseRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/viewcourse/{id}")
+    public String viewCourse(@PathVariable("id") Long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "coursedetail";
+    }
+
+    @PostMapping("/processcourse")
+    public String processCourse(@Valid @ModelAttribute Course course, BindingResult result){
+        if(result.hasErrors()){
+            return "courseform";
+        }
+        courseRepository.save(course);
+        return "redirect:/";
+    }
 
 }
